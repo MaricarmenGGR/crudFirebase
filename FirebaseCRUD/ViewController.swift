682fa4070c947342a00a4331f767c7c569adee4a
Catalina <catalina@iMac-de-Catalina.local>
2020-12-15 17:46:05 -0600

    AppEjemploCrudAvance

--- FirebaseCRUD/ViewController.swift
+++ FirebaseCRUD/ViewController.swift
@@ -11,78 +11,78 @@ import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nombreTF: UITextField!
+    @IBOutlet weak var apellidoTF: UITextField!
+    @IBOutlet weak var apodoTF: UITextField!
+   
+    @IBOutlet weak var usuarios: UITextView!
+    
     var db: Firestore!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let settings = FirestoreSettings()
-
         Firestore.firestore().settings = settings
-                // [END setup]
         db = Firestore.firestore()
-        
-        agregarUsuario()
         
     }
     
-    func leerUsuarios() {
+    @IBAction func saveUser(_ sender: UIButton) {
+        
+        db.collection("users").document(self.apodoTF.text!).setData([
+            "nombre": self.nombreTF.text!,
+            "apellido": self.apellidoTF.text!
+            ]) { err in
+                if let err = err {
+                    print("Error al escribir el Documento: \(err)")
+                } else {
+                    print("El Documento se escribio exitosamente!")
+                }
+            }
+        
+    }
+    
+    @IBAction func deleteUser(_ sender: UIButton) {
+        db.collection("users").document(self.apodoTF.text!).delete() { err in
+            if let err = err {
+                print("No se removio ningun documento: \(err)")
+            } else {
+                print("Documento removido satisfactoriamente")
+            }
+        }
+    }
+    @IBAction func editUser(_ sender: UIButton) {
+        
+        let user = db.collection("users").document(self.apodoTF.text!)
+
+        user.updateData([
+            "nombre": self.nombreTF.text!,
+            "apellido": self.apellidoTF.text!
+        ]) { err in
+            if let err = err {
+                print("Error al editar: \(err)")
+            } else {
+                print("Documento editado")
+            }
+        }
+        
+    }
+    @IBAction func readUser(_ sender: UIButton) {
         db.collection("users").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
+                
                 print("DOCUMENTOS POR USUARIO")
                 for document in querySnapshot!.documents {
+                    self.usuarios.text! += "\(document.data())"+"\n"
                     print("\(document.documentID) => \(document.data())")
+                    
                 }
             }
         }
         
     }
-    
-    func agregarUsuario() {
-        // Add a new document in collection "cities"
-        db.collection("users").document("U1").setData([
-            "first": "Mari",
-            "last": "Rodriguez",
-            "born": 1998
-        ]) { err in
-            if let err = err {
-                print("Error writing document: \(err)")
-            } else {
-                print("Document successfully written!")
-            }
-        }
-    }
-    
-    func borrarUsuarios(){
-        
-        db.collection("users").document("U1").delete() { err in
-            if let err = err {
-                print("Error removing document: \(err)")
-            } else {
-                print("Document successfully removed!")
-            }
-        }
-    }
-    
-    private func updateDocument() {
-            // [START update_document]
-            let user = db.collection("users").document("hKn8iKCDHmL4RyfPKmmA")
-
-            // Set the "capital" field of the city 'DC'
-            user.updateData([
-                "born": 1990
-            ]) { err in
-                if let err = err {
-                    print("Error updating document: \(err)")
-                } else {
-                    print("Document successfully updated")
-                }
-            }
-            
-        }
-
 
 }
 
